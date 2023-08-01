c79549785421193b5a15f9bc6fc9caa795b937b4
Bequi03 <rebecapereyracm@gmail.com>
2023-08-01 16:51:46 -0300

    1 de agosto 16 51

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,4 @@
-let serviciosEnCarrito = localStorage.getItem("servicios-en-carrito");
-serviciosEnCarrito = JSON.parse(serviciosEnCarrito); 
+const serviciosEnCarrito = JSON.parse(localStorage.getItem("servicios-en-carrito"));
 
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
 const contenedorCarritoServicios = document.querySelector("#carrito-servicios");
@@ -9,10 +8,8 @@ let botonesEliminar = document.querySelectorAll("#carrito-servicios-eliminar");
 const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
 const contenedorTotal = document.querySelector("#total");
 
-
-function cargarServiciosCarrito () {
+function cargarServiciosCarrito() {
   if (serviciosEnCarrito && serviciosEnCarrito.length > 0) {
-
     contenedorCarritoVacio.classList.add("disabled");
     contenedorCarritoServicios.classList.remove("disabled");
     contenedorCarritoAcciones.classList.remove("disabled");
@@ -20,85 +17,78 @@ function cargarServiciosCarrito () {
 
     contenedorCarritoServicios.innerHTML = "";
 
-    serviciosEnCarrito.forEach(servicios => {
-            const div = document.createElement("div");
-            div.classList.add("carrito-servicios");
-            div.innerHTML = `
-            <img class="carrito-servicios-imagen" src="${servicios.imagen}" alt="${servicios.titulo}">
-            <div class="carrito-servicios-titulo">
-                <small>Título</small>
-                <h3>${servicios.titulo}</h3>
-            </div>
-            <div class="carrito-servicios-cantidad">
-                <small>Cantidad</small>
-                <p>${servicios.cantidad}</p>
-            </div>
-            <div class="carrito-servicios-precio">
-                <small>Precio</small>
-                <p>$${servicios.precio}</p>
-            </div>
-            <div class="carrito-servicios-subtotal">
-                <small>Subtotal</small>
-                <p>$${servicios.precio * servicios.cantidad}</p>
-            </div>
-            <button class="carrito-servicios-eliminar" id="${servicios.id}"><i class="bi bi-trash-fill"></i></button>
-        `;
-
-        contenedorCarritoServicios.append(div);
-    })
-} else {
-    
+    serviciosEnCarrito.forEach((servicios) => {
+      const div = document.createElement("div");
+      div.classList.add("carrito-servicios");
+      div.innerHTML = `
+        <img class="carrito-servicios-imagen" src="${servicios.imagen}" alt="${servicios.titulo}">
+        <div class="carrito-servicios-titulo">
+          <small>Título</small>
+          <h3>${servicios.titulo}</h3>
+        </div>
+        <div class="carrito-servicios-cantidad">
+          <small>Cantidad</small>
+          <p>${servicios.cantidad}</p>
+        </div>
+        <div class="carrito-servicios-precio">
+          <small>Precio</small>
+          <p>$${servicios.precio}</p>
+        </div>
+        <div class="carrito-servicios-subtotal">
+          <small>Subtotal</small>
+          <p>$${servicios.precio * servicios.cantidad}</p>
+        </div>
+        <button class="carrito-servicios-eliminar" id="${servicios.id}"><i class="bi bi-trash-fill"></i></button>
+      `;
+      contenedorCarritoServicios.append(div);
+    });
+  } else {
     contenedorCarritoVacio.classList.remove("disabled");
     contenedorCarritoServicios.classList.add("disabled");
     contenedorCarritoAcciones.classList.add("disabled");
     contenedorCarritoComprado.classList.add("disabled");
+  }
+  actualizarBotonesEliminar();
+  actualizarTotal();
 }
-actualizarBotonesEliminar();
-actualizarTotal();
-
-}
-
-
-cargarServiciosCarrito ();
-
-
 
 function actualizarBotonesEliminar() {
-    botonEliminar = document.querySelectorAll(".carrito-servicios-eliminar");
-  
-    botonEliminar.forEach((boton) => {
-      boton.addEventListener("click", eliminarDelCarrito);
-    });
-  }
-
-
-  function eliminarDelCarrito(e) {
-    const idBoton = e.currentTarget.id;
-    const index = serviciosEnCarrito.findIndex(servicio => servicio.id === idBoton);
-    serviciosEnCarrito.splice(index, 1);
-    cargarServiciosCarrito();
-    localStorage.setItem("servicios-en-carrito", JSON.stringify(serviciosEnCarrito));
+  botonEliminar = document.querySelectorAll(".carrito-servicios-eliminar");
+  botonEliminar.forEach((boton) => {
+    boton.addEventListener("click", eliminarDelCarrito);
+  });
 }
 
-botonVaciar.addEventListener("click", vaciarCarrito);
+function eliminarDelCarrito(e) {
+  const idBoton = e.currentTarget.id;
+  const index = serviciosEnCarrito.findIndex((servicio) => servicio.id === idBoton);
+  serviciosEnCarrito.splice(index, 1);
+  cargarServiciosCarrito();
+  localStorage.setItem("servicios-en-carrito", JSON.stringify(serviciosEnCarrito));
+}
 
 function vaciarCarrito() {
   serviciosEnCarrito.length = 0;
   localStorage.setItem("servicios-en-carrito", JSON.stringify(serviciosEnCarrito));
   cargarServiciosCarrito();
 }
+
 function actualizarTotal() {
-  const totalCalculo = serviciosEnCarrito.reduce((acc, servicio) => acc + (servicio.precio * servicio.cantidad), 0);
+  const totalCalculo = serviciosEnCarrito.reduce((acc, servicio) => acc + servicio.precio * servicio.cantidad, 0);
   contenedorTotal.innerText = `$${totalCalculo}`;
 }
 
-  botonComprar.addEventListener("click", comprarCarrito);
-function comprarCarrito() {
-    serviciosEnCarrito.length = 0;
-    localStorage.setItem("servicios-en-carrito", JSON.stringify(serviciosEnCarrito));
+const botonComprar = document.querySelector("#boton-comprar");
+botonComprar.addEventListener("click", comprarCarrito);
 
-    contenedorCarritoVacio.classList.add("disabled");
-    contenedorCarritoServicios.classList.add("disabled");
-    contenedorCarritoAcciones.classList.add("disabled");
-    contenedorCarritoComprado.classList.remove("disabled");
+function comprarCarrito() {
+  serviciosEnCarrito.length = 0;
+  localStorage.setItem("servicios-en-carrito", JSON.stringify(serviciosEnCarrito));
+
+  contenedorCarritoVacio.classList.add("disabled");
+  contenedorCarritoServicios.classList.add("disabled");
+  contenedorCarritoAcciones.classList.add("disabled");
+  contenedorCarritoComprado.classList.remove("disabled");
 }
+
+cargarServiciosCarrito();
